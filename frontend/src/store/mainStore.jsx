843bf404b7e9ca1fe085a7ab29ed15d1b4604ea3
aliasgar1099 <aliasgar1099@gmail.com>
2023-02-25 14:59:31 +0300

    New Secret key generation

--- frontend/src/store/mainStore.jsx
+++ frontend/src/store/mainStore.jsx
@@ -2,9 +2,10 @@ import { create } from 'zustand'
 const initialState = {
     bears : 0
 }
-const useMainStore = create((set) => ({
+export const useMainStore = create((set) => ({
     // Initial Values
     ...initialState,
     increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
     removeAllBears: () => set({ bears: 0 }),
 }))
+
