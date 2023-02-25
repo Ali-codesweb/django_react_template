29e959c72c0e269e52d6f664addd43ff3ba8943a
aliasgar1099 <aliasgar1099@gmail.com>
2023-02-25 20:56:14 +0300

    Added cors headers

--- django_react_template/settings.py
+++ django_react_template/settings.py
@@ -71,6 +71,7 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
