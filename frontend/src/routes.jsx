420b02e0a4ab7ecb45a5383c036434cd36562e8f
unknown <aliasgar1099@gmail.com>
2023-12-14 22:43:51 +0300

    refreshed frontend packages and code

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,6 +1,8 @@
+import App from "./App";
+import { createBrowserRouter } from "react-router-dom";
 export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <div>Hello world!</div>,
-    },
-  ]);
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
