213543f71b569e7fe97a487c3ec7c7aa850788f9
unknown <aliasgar1099@gmail.com>
2023-12-14 21:28:21 +0300

    default

--- oddroo_backend/urls.py
+++ oddroo_backend/urls.py
@@ -1,4 +1,4 @@
-"""django_react_template URL Configuration
+"""oddroo_backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
